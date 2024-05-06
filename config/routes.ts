94803935eb5fc6ee56a622711ed3ac019f11695d
Andy <Andy.8866@outlook.com>
2024-05-06 09:37:19 +0800

    优化

--- config/routes.ts
+++ config/routes.ts
@@ -1,6 +1,4 @@
-﻿import TransferList from "@/pages/TransferList";
-
-/**
+﻿/**
  * @name umi 的路由配置
  * @description 只支持 path,component,routes,redirect,wrappers,name,icon 的配置
  * @param path  path 只支持两种占位符配置，第一种是动态参数 :id 的形式，第二种是 * 通配符，通配符只能出现路由字符串的最后。
